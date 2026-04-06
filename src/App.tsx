3b6c51e7c5c9e02639f8ee3a3fc792fdd980c162
Robin Ryan <robin1ryan@hotmail.com>
2026-04-06 15:48:29 +0300

    built a generic basic fetcher for obtaining data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const ACCESS_TOKEN = import.meta.env.VITE_TOKEN;
+const LINK = import.meta.env.VITE_LINK;
+
+const useFetchData = (link, ...importSettings) => {
+  const [error, setError] = useState<string | Error | null>(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchFn = async () => {
+      const signal = controller.signal;
+      setIsLoading(true);
+
+      try {
+        const res = await fetch(link, { ...importSettings, signal });
+
+        if (!res.ok) {
+          setError("Req was not ok");
+        }
+
+        const fetchedData = await res.json();
+        setData(fetchedData);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          setError("Fetch aborted");
+        } else {
+          setError("Fetched failed: ", err);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchFn();
+
+    return () => controller.abort();
+  }, [link]);
+
+  return [error, data, isLoading];
+};
 
 const SearchBar = () => {
   return <h1>here we search</h1>;
 };
 
 function App() {
+  const [data, error, isLoading] = useFetchData(LINK, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+  });
+
   return (
     <div>
       <SearchBar />
